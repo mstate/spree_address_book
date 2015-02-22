cf235287ebed0e3f7762475f116389cdf5e75816
mike <mstate@gmail.com>
2015-02-22 11:24:01 -0500

    fix logic on spree admin's adress use_billing checkbox

---
+++ app/overrides/fix_address_checkbox_bug_in_spree.rb
@@ -0,0 +1,18 @@
+# fixes logic of when to show checked which appears to be a bug in spree.
+# also sends a value of 0 when unchecked
+
+text_to_replace = %{
+	              <span data-hook="use_billing">
+	              	<input name="order[use_billing]" type="hidden" value="0" />
+                  <%= check_box_tag 'order[use_billing]', '1', @order.bill_address.empty? || @order.ship_address.empty? || @order.bill_address.id == @order.ship_address.id) %>
+                  <%= label_tag 'order[use_billing]', Spree.t(:use_billing_address) %>
+                </span>
+} 
+
+
+Deface::Override.new(
+  :virtual_path => "spree/admin/orders/customer_details/_form",
+  :name => "fix_checkbox_logic_on_admin_address",
+  :replace => "[data-hook='use_billing']",
+  :text => text_to_replace
+)
